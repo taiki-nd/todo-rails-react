26d93f3aa342308f28ee6408ee0f35ece1c14de7
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-28 20:48:34 +0900

    ナビゲーションバーの作成

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import styled from 'styled-components'
+import { AddTodo } from './AddTodo'
+import { EditTodo } from './EditTodo'
+import { TodoList } from './TodoList'
+import './App.css'
+import { Switch, Route, Link } from 'react-router-dom'
 
 export const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Navbar>
+        <Logo>ToDoList</Logo>
+        <Actions>
+          <Link to="/todos">
+            <Action>ToDoLists</Action>
+          </Link>
+          <Link to="/todos/new">
+            <Action>NewTodo</Action>
+          </Link>
+        </Actions>
+      </Navbar>
+    </>
   )
 }
+
+const Navbar = styled.nav`
+  background-color: #ccffcc;
+  min-height: 8vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  letter-spacing: 3px;
+`
+
+const Actions = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`
+
+const Action = styled.li`
+font-weight: bold;
+font-size: 20px;
+opacity: 0.7;
+&:hover {
+  opacity: 1;
+}
+`
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20px auto;
+`
