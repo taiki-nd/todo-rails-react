c0576e7a5ddbcb2f83e19794f1e0bfd059aff288
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-28 21:41:59 +0900

    検索窓関連の作成

--- app/javascript/components/TodoList.js
+++ app/javascript/components/TodoList.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const TodoList = () => {
   return (
-    <div>
-      TodoList
-    </div>
+    <>
+      <h1>ToDo Lists</h1>
+      <InputAndRemoveAll>
+        <Input />
+        <RemoveAllBtn>RemoveAll</RemoveAllBtn>
+      </InputAndRemoveAll>
+      <div>
+        
+      </div>
+    </>
   )
 }
 
+const InputAndRemoveAll = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Input = styled.input`
+  font-size: 20px;
+  width: 100%;
+  height: 40px;
+  margin: 10px 0;
+  padding: 10px;
+`
+
+const RemoveAllBtn = styled.button`
+  width: fit-content;
+  height: 40px;
+  background: #cc3366;
+  border: none;
+  font-weight: 500;
+  margin-left: 10px;
+  padding: 5px 10px;
+  border-radius: 3px;
+  color: #fff;
+  cursor: pointer;
+`
