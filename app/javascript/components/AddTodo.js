a44e613a8eda700155d3d033b1a8648d6a9a937f
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-29 10:31:47 +0900

    toastの有効化

--- app/javascript/components/AddTodo.js
+++ app/javascript/components/AddTodo.js
@@ -5,6 +5,8 @@ import { TodoList } from './TodoList'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+toast.configure();
+
 export const AddTodo = (props) => {
 
   const initialTodoStatus = {
