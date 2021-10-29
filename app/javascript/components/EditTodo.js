9fe76e71a91f183c6a663b7cee26d267bdd7320c
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-29 11:59:44 +0900

    見た目の準備

--- app/javascript/components/EditTodo.js
+++ app/javascript/components/EditTodo.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const EditTodo = () => {
   return (
-    <div>
-      EditTodo
-    </div>
+    <>
+      <h1>EditTodo</h1>
+      <h2>CurrentTodo</h2>
+      <input 
+        type="text"/>
+      
+      <button>修正</button>
+      <h2>CurrentStatus</h2>
+      <div>
+        Complete Incomplete
+      </div>
+      <h2>EditStatus</h2>
+      <div>
+        <button>完了</button>
+        <button>未完了</button>
+        <button>削除</button>
+      </div>
+    </>
   )
 }
