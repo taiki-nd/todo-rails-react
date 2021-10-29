d8a87ded00e32c6dab8b1e037a273e7b7e9f1044
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-29 21:50:18 +0900

    修正

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { AddTodo } from './AddTodo'
 import { EditTodo } from './EditTodo'
 import { TodoList } from './TodoList'
+import './App.css'
 import { Switch, Route, Link } from 'react-router-dom'
 
 export const App = () => {
